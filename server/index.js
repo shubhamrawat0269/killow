8c22910193192fb7070651cb6f41cf4e86acf1e0
shubham Rawat <rawatshubam269@gmail.com>
2024-06-20 22:53:15 +0530

    Database Connectivity with layered Structure

--- server/index.js
+++ server/index.js
@@ -2,6 +2,20 @@ const express = require("express");
 const PORT = 8082;
 const app = express();
 
-app.listen(PORT, () => {
-  console.log(`Server Started at PORT ${PORT}`);
-});
+var cors = require("cors");
+require("dotenv").config();
+
+const cookiesParser = require("cookie-parser");
+const connectToDb = require("./db/connectToDb");
+
+app.use(cors());
+app.use(express.json());
+app.use(cookiesParser());
+
+app.use(express.urlencoded({ extended: true }));
+
+connectToDb();
+
+app.listen(process.env.PORT, () =>
+  console.log(`Server Started at PORT ${process.env.PORT}`)
+);
